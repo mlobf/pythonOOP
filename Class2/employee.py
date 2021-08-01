7ed47e622c353f26a00ce2ddef901796ea7ca469
mlobf <marcoslemeborbafilho@gmail.com>
2021-08-01 16:34:11 -0300

    Add private method and classes

---
+++ Class2/employee.py
@@ -0,0 +1,41 @@
+class Employee:
+    """
+        Class name, short name is better.
+    """
+
+    def __init__(self, name, age, level='junior'):
+        self._name = name
+        self._age = age
+        self._level = level
+        self._salary = self._compute_salary()
+    '''
+    def promete(self):
+        if self._level == 'junior':
+            self._level == 'senior'
+        elif self._level == 'senior':
+            self._level == 'CEO'
+        self._compute_salary()
+    '''
+
+    def _compute_salary(self):
+        if self._level == 'junior':
+            return 10000
+        elif self._level == 'senior':
+            return 20000
+        elif self._level == 'CEO':
+            return 1000000
+        else:
+            print('unknown level')
+
+    def get_salary(self):
+        return self._salary
+
+    def get_name(self):
+        return self._name
+
+    def get_age(self):
+        return self._age
+
+        # Private atribute.
+        # Python hands encapsulation with:
+        # _name_of_varible or _name_of_method.
---
+++ Class2/main.py
@@ -0,0 +1,9 @@
+from employee import Employee
+
+
+e1 = Employee('Alex', 25, 'junior')
+
+
+print(f'Employee s salary is {e1.get_salary()}')
+
+# print(e1._compute_salary('CEO'))
