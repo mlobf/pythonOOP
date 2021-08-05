c6e60b33911e01e1ee5689bcdbb5b3c478b9f1c5
mlobf <marcoslemeborbafilho@gmail.com>
2021-08-05 18:12:31 -0300

    static method

--- Class3/class_methods.py
+++ Class3/class_methods.py
@@ -10,5 +10,22 @@ class Apple:
         self.color = color
 
     @classmethod
-    def set_calories(cls, calories):
-        pass
+    def _set_calories(cls, c):
+        print(f'cls is of type {type(cls)} and id {id(cls)}')
+        cls.calories = c
+
+    @staticmethod
+    def volume(r):
+        return 4/3*3.14*(r**3)
+
+
+a = Apple('red', 100)
+print(f'id of Apple class : {id(Apple)}')
+print(f'id of Apple instance a : {id(Apple)}')
+
+
+Apple._set_calories(100)
+
+print(
+    a.volume(10)
+)
