eb729fe33e6de416be18fa40c0e2e84bc0040cce
mlobf <marcoslemeborbafilho@gmail.com>
2021-08-15 23:25:10 -0300

    Review of generators

---
+++ my_generator/my_simple_generator.py
@@ -0,0 +1,29 @@
+import sys
+
+"""
+    Generators are  expert feature of python,
+        They help's us to deal better with the
+        computers RAM memory.
+
+    Tips:   yield => pause
+            return => stop
+"""
+
+
+def gen(n):
+    for i in range(n):
+        yield i**3
+
+
+x = [i**2 for i in range(10000)]
+g = gen(100000)
+
+
+print(sys.getsizeof(x))
+print(sys.getsizeof(g))
+
+
+'''
+for i in g:
+    print(i)
+'''
